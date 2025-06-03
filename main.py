072fe921d039c3a14210197f2729ca92eacf1631
M4lfestio <lrasztovits@gmail.com>
2025-06-03 19:42:10 +0200

    Added asteroid splitting

--- main.py
+++ main.py
@@ -43,7 +43,7 @@ def main():
             for shot in shots:
                 if asteroid.collides(shot):
                     shot.kill()
-                    asteroid.kill()
+                    asteroid.split()
         for sprite in drawable:
             sprite.draw(screen)
         pygame.display.flip()
